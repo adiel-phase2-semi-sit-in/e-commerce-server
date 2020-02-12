c08bbfb1ac0ba11bd3b9fd6d75454198f154bc16
adilpratama <adilpratama71@yahoo.com>
2020-02-12 23:02:43 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+if (process.env.NODE_ENV == "development") {
+  require("dotenv").config();
+}
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
